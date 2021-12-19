59fa1c16d92010f0742f96f01aee1659f75b8cb3
fnaohisa <71742361+fnaohisa@users.noreply.github.com>
2021-12-19 23:04:59 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,381 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>キョードー東京「バック・トゥ・ザ・フューチャー」inコンサート/シネオケ®</title>
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link rel="shortcut icon" href="img/favicon.ico"/>
+<meta name="format-detection" content="telephone=no">
+<meta name="description" content="大ヒット映画「バック・トゥ・ザ・フューチャー」の１作目を全編フルオーケストラ生演奏付きで見るコンサート/シネオケ®が、平成最後の日に開催決定！">
+<meta name="keywords" content="キョードー東京,バック・トゥ・ザ・フューチャー,コンサート,シネオケ">
+<meta property="og:title" content="キョードー東京「バック・トゥ・ザ・フューチャー」inコンサート/シネオケ®">
+<meta property="og:description" content="大ヒット映画「バック・トゥ・ザ・フューチャー」の１作目を全編フルオーケストラ生演奏付きで見るコンサート/シネオケ®が、平成最後の日に開催決定！">
+<meta property="og:site_name" content="キョードー東京「バック・トゥ・ザ・フューチャー」inコンサート/シネオケ®">
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://cineoke.info/on/bttf">
+<meta property="og:image" content="img/ogp.png">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">
+<link rel="stylesheet" href="scss/common.css">
+<link rel="stylesheet" href="scss/utility.css">
+<link rel="stylesheet" href="scss/style.css">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-135453797-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-135453797-1');
+</script>
+<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '2295683553786980');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=2295683553786980&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->  
+</head>
+<body>
+  <header class="header">
+    <p class="header__twitter">
+      <a href="https://twitter.com/bttf_in_concert" target="_blank">
+        <img src="img/fv_twitter.png" alt="twitter">
+      </a>
+    </p>
+  </header>
+  <section class="fv">
+    <div class="fv__inner inner">
+      <p class="fv__lead oswald"></p>
+      <h2 class="fv__title">&nbsp;<br>&nbsp;<br>&nbsp;
+      </h2>
+      <p class="fv__logo  animation_top">
+        <img src="img/fv_title_01.png" alt="JAWS">
+      </p>
+      <p class="fv__logo--under  animation_top_2">
+        <img src="img/fv_title_02.png" alt="inコンサート2019">
+      </p>
+      <div>
+        <ul>
+        <li style="width: 45%; display: inline-block;">
+          <h2 class="fv__title"><span>この夏、コンサート会場に</span><br><span>「ジョーズ」がやってくる！</span><br><span>映画全編生演奏付き上映シネマオーケストラ</span><br><span>コンサート（シネオケ®)で</span><br><span>臨場感たっぷりの恐怖を味わおう！</span>
+          </h2>
+        </li>
+        <li style="width: 50%; display: inline-block;">
+          <img src="img/jaws.png" alt="inコンサート2019">
+        </li>
+        </ul>
+      </div>
+      <p class="fv_copy">™ &amp; © Universal Studios and U-Drive Joint Venture</p>
+    </div>
+  </section>
+  
+  
+  <section class="date animation">
+    <div class="date__inner inner">
+      <h2 class="date__logo">
+        <img src="img/date-pc.png" alt="2019年8月3日(土)" class="u-for-pc"><img src="img/date-sp.png" alt="2019年8月3日(土)" class="u-for-sp">
+      </h2>
+      <div class="cvr_btn__wrapper">
+        <p class="cvr_btn">
+          <a href="https://ticket.kyodotokyo.com/bttf.do" target="_blank">チケット購入はこちら</a>
+        </p>
+      </div>
+    </div>
+  </section>
+
+<div class="overview04 animation">  
+  <section class="overview">
+    <div class="inner">
+      <div class="overview__content">
+        <p class="overview__tmb">
+          <img src="img/overview-tmb.png" alt="">
+        </p>
+        <p class="overview__text text">
+          「ジョーズ」in コンサートは、舞台上の大スクリーンにて映画丸々一本を上映し、サウンドトラック部分を舞台上のフルオーケストラが全編を通して演奏する。<br>「ジョーズ」は1975年製作当時、弱冠27歳だったスティーヴン・スピルバーグ監督が、ピーター・ベンチュリーの同名小説を映画化し大ヒットとなった海洋パニック。本作品で２度目のアカデミー賞を受賞したジョン・ウィリアムズのメインテーマは、誰が聴いてもすぐにそれとわかる名曲中の名曲だ。それらの音楽を「ジョーズ」in コンサートでは臨場感たっぷりの生演奏で味わうことができる。<br>コントラバスの低音が不気味にゆっくり奏でられて、恐怖が始まる瞬間。夏の海の香りが届く会場で、映画史上に残る素晴らしい音楽と映画を、映画全編生演奏付き上映 (シネオケ(R)) で体感しよう！     
+        </p>
+      </div>
+    </div>
+  </section>
+  
+  <div class="movie">
+    <div class="inner">
+      <div class="movie__wrapper">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/vH5Kve9c218"  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+
+ 
+  <div class="info">
+    <div class="inner">
+        <dl class="ticket animation">
+          <dt class="ticket__title title">
+            チケット最速先行販売
+          </dt>
+          <dd class="ticket__text text">
+            <span class="u-text-bold">オフィシャルサイト先着先行受付</span><br>
+            3月19日（火）お昼12:00より開始<br>
+            申込先： ＠＠＠＠＠＠＠＠
+          </dd>
+        </dl>
+
+        <div class="message__content">
+          <h3 class="message__content__title">作曲家、ジョン・ウィリアムズよりメッセージ</h3>
+          <div class="message__content__detail">
+            <p class="message__content__tmb">
+              <img src="img/message_01.png" alt="ジョン・ウィリアムズ">
+              <span class="text--sub">©&nbsp;Lefterisphoto.com</span>
+            </p>
+            <div class="message__content__text">
+              <p class="text">
+                最初に「ジョーズ」を見た時、<br>
+                私はオーケストラ音楽が映画を印象付ける役割と効果を発揮した<br>
+                素晴らしい機会であったとはっきりとわかっていませんでした。<br>
+                素晴らしい機会であったとはっきりとわかっていませんでした。<br>
+                ”ズンズン、ズンズン、ズンズンズンズンズンズン"のメインテーマに<br>
+                懐疑的だったのですが、とても上手くいったようです。<br>
+                私たち二人にとって、この他に類を見ない特殊な映画が、今もなお皆さん<br>
+                の脳裏に残っていると知って感動しています。<br>
+                そして、映画公開から40年以上経った今、<br>
+                オーケストラの生演奏付きで映画を楽しんで頂くこのコンサートは、<br>
+                ジョーズの音楽を作曲した私にとって、最高のご褒美であり喜びです。<br>
+                是非お楽しみください。<br>
+              </p>
+              <div class="message__content__sub">
+                <p>ジョン・ウィリアムズ</p>
+                <p><img src="img/sign.png" alt=""></p>
+                <!--<p><img src="img/sign.png" alt=""></p>-->
+              </div>
+            </div>
+          </div>
+        </div>
+      
+        <dl class="schedule">
+          <dt class="schedule__title title">
+            公演内容＆スケジュール
+          </dt>
+          <dd class="schedule__text text">
+            <p class="schedule__text--em">ジョーズ in コンサート</p>            
+            <p class="u-mt-2">
+              作曲＝ジョン・ウィリアムズ<br>
+              指揮：齊藤一郎<br>
+              演奏：神奈川フィルハーモニー管弦楽団<br>
+              <span class="text--mini">＊英語上映・日本語字幕あり</span>
+            </p>
+            
+            <div class="schedule__detail">
+              <div class="schedule__detail__title u-text-bold">公演日時＆会場</div>
+              <div class="schedule__detail__text">
+                2019年8月3日(土) <br>
+                17時開演（16時開場）<br>
+                パシフィコ横浜 国立大ホール<a href="http://www.nhk-sc.or.jp/nhk_hall/access.html" target="_blank" class="box">アクセス</a><br>
+                交通：みなとみらい線「みなとみらい駅」より徒歩5分
+              </div>
+            </div>
+            
+            <div class="schedule__detail">
+              <div class="schedule__detail__title u-text-bold">チケット情報</div>
+              <div class="schedule__detail__text">
+                <p>
+                  料金（税込）：<br>
+                  大人S席 9,800円&nbsp;&nbsp;&nbsp;小・中人S席6,800円<br>
+                  大人A席 7,800円&nbsp;&nbsp;&nbsp;小・中人A席4,800円<br>
+                </p>
+                <p class="text--mini u-mt-4">
+                  ※小・中人は5歳～17歳まで。入場時に年齢の分かる証明書のご提示をお願いする場合がございます。<br>
+                  ※５歳未満のお子様はご入場できません。<br>
+                  ※車イス席をご利用ご希望のお客様はS席をご購入いただき、キョードー東京( 0570-550-799)までお電話にてご連絡ください。<br>
+                  ※チケットを紛失された方、または当日チケットをお忘れになった方はチケットがなければどのような場合でもご入場できません。<br>
+                  ※ジョン・ウィリアムズは出演致しません。<br>
+                </p>
+                <p class="text--mini u-mt-4">
+                  ※上演時間…約2時間30分予定（休憩20分含む）                
+                </p>
+              </div>
+            </div>
+            
+            <div class="schedule__detail">
+              <div class="schedule__detail__title u-text-bold">
+                お問い合わせ・<br>ご予約・団体販売<br>(10名以上)
+              </div>
+              <div class="schedule__detail__text">
+                キョードー東京0570-550-799<br>
+                （平日11:00〜18:00／土日祝10:00〜18:00）               
+              </div>
+            </div>
+            
+            <p class="u-text-bold text u-mt-10">主催・企画・招聘：キョードー東京</p>
+            
+          </dd>
+        </dl>
+    </div>
+    
+    <div class="cvr_btn01__wrapper u-mt-4">
+      <p class="cvr_btn01">
+        <a href="https://ticket.kyodotokyo.com/bttf.do" target="_blank">チケット購入はこちら</a>
+      </p>
+    </div>
+    
+  </div>
+
+
+
+  
+  <section class="performer animation">
+    <div class="inner">
+      <h2 class="performer__title title u-text-bold">出演者・クリエイター</h2>
+      
+      <div class="performer__content">
+        <h3 class="performer__content__title">作曲<span>Music</span></h3>
+        <div class="performer__content__detail">
+          <p class="performer__content__name u-for-sp">
+            ジョン・ウィリアムズ<br>
+            <span>JOHN WILLIAMS</span>
+          </p>
+          <p class="performer__content__tmb">
+            <img src="img/performer_01.png" alt="ジョン・ウィリアムズ">
+          </p>
+          <div class="performer__content__text">
+            <p class="performer__content__name u-for-pc">
+              ジョン・ウィリアムズ<br>
+              <span>JOHN WILLIAMS</span>
+            </p>
+            <p class="text--mid u-mt-2">
+              ジョン・ウイリアムズは、50年以上もの間、活躍しているアメリカで最も成功している映画作曲家。<br>
+              「スター・ウォーズ」全8作、「ハリー・ポッター」の最初の3作、「スーパーマン」「SAYURI」「ホーム・アローン」「やさしい本泥棒」などを含む100作以上の映画の音楽を作曲している。映画監督スティーヴン・スピルバーグとの45年に渡る芸術においての関係は、「シンドラーのリスト」「E.T.」「ジョーズ」「ジュラシック・パーク」「未知との遭遇」「インディ・ジョーンズ」「プライベート・ライアン」「リンカーン」「BFG」「ペンタゴン・ペーパーズ 最高機密文書」などハリウッドを代表するたくさんの映画を世に送り出した。<br>
+              映画音楽以外の作曲も多く行っており、４つのオリンピック(1983夏ロサンゼルス、1988夏ソウル、1996夏アトランタ、2002冬ソルトレークシティ）のテーマ曲、２つの交響曲、協奏曲を作曲、ボストン・ポップス・オーケストラの音楽監督を14シーズン務め、現在も名誉指揮者として名を連ねる。<br>
+              アカデミー作曲賞においては、今までに51度のノミネート、5度の受賞を誇り、7度のイギリスのアカデミー賞、24度のグラミー賞、４度のゴールデン・グローブ賞、そして５度のエミー賞を獲得。
+              2003年、国際オリンピック委員会（IOC）から、ジョンのオリンピック運動への貢献に対して最も名誉なオリンピック 功労賞が贈られた。<br>
+              2009年にはアメリカ政府から全米芸術勲章を授与、2016年には、アメリカ映画協会（AFI）より44回生涯功労賞を授与。作曲家として、初めての受賞となった。
+            </p>
+          </div>
+        </div>
+      </div>
+      
+      <div class="performer__content">
+        <h3 class="performer__content__title">指揮<span>Conductor</span></h3>
+        <div class="performer__content__detail">
+          <p class="performer__content__name u-for-sp">
+            齊藤一郎<br>
+            <span>ICHIRO SAITO</span>
+          </p>
+          <p class="performer__content__tmb">
+            <img src="img/performer_02.png" alt="齊藤一郎">
+          </p>
+          <div class="performer__content__text">
+            <p class="performer__content__name u-for-pc">
+              齊藤一郎<br>
+              <span>ICHIRO SAITO</span>
+            </p>
+            <p class="text--mid u-mt-2">
+              東京学芸大学、及び東京藝術大学音楽学部指揮科卒業。東京藝大在学中に安宅賞受賞。1997年大阪センチュリー交響楽団を指揮してデビュー。1998年より文化庁新進芸術家海外研修員としてウィーンで研鑽を積んだ後、NHK交響楽団アシスタントコンダクターを務めた。これまでに、Ｎ響等国内主要オーケストラに客演する他、セゲト響（ハンガリー）、モラヴィア・フィル（チェコ）、オラディア・フィル（ルーマニア）などにも出演。2009年4月～2014年3月セントラル愛知交響楽団常任指揮者。2014年4月より同団首席客演指揮者。2014年4月より京都フィルハーモニー室内合奏団音楽監督。邦人作品への積極的な取り組みが評価され、2014年第9回名古屋音楽ペンクラブ賞を受賞。
+            </p>
+          </div>
+        </div>
+      </div>
+      
+      <div class="performer__content">
+        <h3 class="performer__content__title">管弦楽<span>Orchestra</span></h3>
+        <div class="performer__content__detail">
+          <p class="performer__content__name u-for-sp">
+            神奈川フィルハーモニー管弦楽団<br>
+            <span>KANAGAWA PHILHARMONIC ORCHESTRA</span>
+          </p>
+          <p class="performer__content__tmb">
+            <img src="img/performer_03.png" alt="神奈川フィルハーモニー管弦楽団">
+          </p>
+          <div class="performer__content__text">
+            <p class="performer__content__name u-for-pc">
+              神奈川フィルハーモニー管弦楽団<br>
+              <span>KANAGAWA PHILHARMONIC ORCHESTRA</span>
+            </p>
+            <p class="text--mid u-mt-2">
+              1970年に発足。1978年7月に財団法人、2014年4月公益財団法人に認定された。神奈川県の音楽文化創造をミッションとして、神奈川県全域を中心に幅広い活動を続けている。<br>
+              県内各地で特別演奏会を開催し、オペラ、バレエ、レコーディングなどに数多く出演。音楽教育にも積極的で、子どものためのコンサートを各地で開催。神奈川フィルのメンバーと児童・生徒との音楽的交流を通して音楽の魅力を伝えるとともに次代の神奈川フィルのファンを増やしていく取り組みとして好評を得ている。これまでに「安藤為次教育記念財団記念賞」(1983)、「神奈川文化賞」(1989)、「NHK地域放送文化賞」、「横浜文化賞」(2007)を受賞。近年では、映像配信やソーシャルネットワークサービス（SNS）など新しいメディアにも意欲的に取り込み、新聞にもたびたび取り上げられるなど多方面で注目を集めている。名誉指揮者に現田茂夫、特別客演指揮者に小泉和裕、常任指揮者に川瀬賢太郎を擁しており、2020年には楽団創立50周年を控え、ますます注目が集まっている。
+            </p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </section>
+</div>
+  
+  <section class="summary">
+    <div class="inner">
+      <h2 class="title">あらすじ</h2>
+      <p class="text">
+        海水浴客でにぎわう夏の海に、突如として巨大な人食いザメが出現。<br>若い女性が無残に食い殺される。<br>警察署長のブロディは海水浴場の閉鎖を訴えるが、<br>町の財政は夏の観光で成り立っているため、意見を聞き入れてもらうことができない。<br>すると第2、第3の犠牲者が発生し、町はたちまちパニックに陥る。<br>ブロディは若き海洋学者のフーパーと荒くれ者の地元の漁師クイントとともに、<br>独断でサメ退治に乗り出す。
+      </p>
+    </div>
+  </section>
+  
+  <section class="data animation">
+    <h2 class="title">映画作品データ</h2>
+    <div class="inner">
+
+      <div class="data__content">
+        <div class="data__detail">
+          <dl class="text">
+            <dt>製作年</dt>
+            <dd>1975年</dd>
+            <dt>製作国</dt>
+            <dd>アメリカ</dd>
+            <dt>監督</dt>
+            <dd>スティーブン・スピルバーグ</dd>
+            <dt>原作・脚本</dt>
+            <dd>ピータ・ベンチリー</dd>
+            <dt>音楽</dt>
+            <dd>ジョン・ウィリアムズ</dd>
+            <dt>キャスト</dt>
+            <dd>
+              ロバート・ショウ、<br>
+              ロイ・シャイダー、<br>
+              リチャード・ドレイファス、<br>
+              リチャード・ドレイファス、<br>
+              マーレイ・ハミルトン&nbsp;他
+            </dd>
+          </dl>
+            <p class="data__sub">第48回 アカデミー賞（1976年）３部門受賞（作曲賞、録音賞、編集賞）</p>
+        </div>
+        <p class="data__tmb">
+          <img src="img/data_tmb.png" alt="JAWS">
+        </p>
+      </div>
+      <p class="note">™ &amp; © Universal Studios and U-Drive Joint Venture</p>
+    </div>
+  </section>
+
+  <footer class="footer">
+    <!--<p class="mask animation_footer">&nbsp;</p>-->
+    <p class="footer__copy text--mini">
+      Copyright © KYODO TOKYO INC. All Rights Reserved.
+    </p>
+  </footer>
+  
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="js/jquery.inview.min.js"></script>
+<script src="js/function.js"></script>
+  
+<!-- Twitter universal website tag code -->
+<script>
+!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+// Insert Twitter Pixel ID and Standard Event data below
+twq('init','o1gvb');
+twq('track','PageView');
+</script>
+<!-- End Twitter universal website tag code -->
+  
+</body>
+</html>
